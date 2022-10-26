0e5ce7e0d51f030a724b09b02d776233bcdd6285
DavidMGomez <david-gomez12@hotmail.com>
2022-10-26 13:25:56 -0500

    classifyDocWithId

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+from google.cloud import storage
 import classify_service
 import json
+import io
 
 clasify_serv = classify_service.ClasifyService()
+bucket = "xirius-landing-ocr-test"
+base = "detect"
+storage_client = storage.Client()
 
 app = Flask(__name__)
 CORS(app)
+
+
+def download_as_blob(bucket_name, source_blob_name):
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.get_blob(source_blob_name)
+    bytes = blob.download_as_bytes()
+    print('Object [' + bucket_name + ':' + source_blob_name + '] downloaded')
+    return bytes, blob
+
 
 @app.route('/')
 def display_default():
@@ -27,6 +41,27 @@ def clasifyPages():
     seiz_response = request.json
     resp = clasify_serv.classify(seiz_response)
     return jsonify(resp)
+
+@app.route('/classifyDocWithId', methods=['POST'])
+def clasifyPagesWithId():
+    resp = {}
+    req_json = request.json
+    uuid = req_json["uuid"]
+    print(uuid)
+    blobs = storage_client.list_blobs(bucket, prefix=base+"/"+uuid)
+    blob = None
+    for b in blobs:
+        print(b)
+        if ".json" in b.name and not "google" in b.name:
+            blob = b 
+    if blob is not None:
+        bytes = download_as_blob(bucket, blob.name)[0]
+        response_json = json.load(io.BytesIO(bytes))
+        print(response_json)
+        resp = clasify_serv.classify(response_json)
+        return jsonify(resp)
+    return jsonify("{}")
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5500, debug=True)
