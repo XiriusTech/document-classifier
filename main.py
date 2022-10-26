b1e426d1aed91738a43b8df7c109a6a45f868db8
DavidMGomez <david-gomez12@hotmail.com>
2022-10-26 13:37:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import NO
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -6,6 +7,8 @@ from google.cloud import storage
 import classify_service
 import json
 import io
+import time
+import datetime
 
 clasify_serv = classify_service.ClasifyService()
 bucket = "xirius-landing-ocr-test"
@@ -26,13 +29,13 @@ def download_as_blob(bucket_name, source_blob_name):
 
 @app.route('/')
 def display_default():
-	return 'Welcome to the semantic search app!\n' \
-				 ''
+    return 'Welcome to the semantic search app!\n' \
+        ''
 
 
 @app.route('/readiness_check')
 def check_readiness():
-	return 'App is ready!'
+    return 'App is ready!'
 
 
 @app.route('/classifyDoc', methods=['POST'])
@@ -42,18 +45,24 @@ def clasifyPages():
     resp = clasify_serv.classify(seiz_response)
     return jsonify(resp)
 
+
 @app.route('/classifyDocWithId', methods=['POST'])
 def clasifyPagesWithId():
     resp = {}
     req_json = request.json
     uuid = req_json["uuid"]
     print(uuid)
-    blobs = storage_client.list_blobs(bucket, prefix=base+"/"+uuid)
     blob = None
-    for b in blobs:
-        print(b)
-        if ".json" in b.name and not "google" in b.name:
-            blob = b 
+    time_init = datetime.datetime.now()
+    while blob is None and (datetime.datetime.now()-time_init).seconds / 60 < 7:
+        blobs = storage_client.list_blobs(bucket, prefix=base+"/"+uuid)
+        for b in blobs:
+            print(b)
+            if ".json" in b.name and not "google" in b.name:
+                blob = b
+        if(blob is None):
+            time.sleep(60*4)
+
     if blob is not None:
         bytes = download_as_blob(bucket, blob.name)[0]
         response_json = json.load(io.BytesIO(bytes))
@@ -64,4 +73,4 @@ def clasifyPagesWithId():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5500, debug=True)
+    app.run(host='0.0.0.0', port=5500, debug=True)
